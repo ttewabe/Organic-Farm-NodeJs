78142296a441bf789a881fbb02f66f426115dcfb
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-07-22 16:38:40 -0700

    update

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
-
-/////////////////////////////////
-// FILES
-
-// Blocking, synchronous way
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-// console.log(textIn);
-// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('File written!');
-
-// Non-blocking, asynchronous way
-// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-//   if (err) return console.log('ERROR! 💥');
-
-//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//     console.log(data2);
-//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-//       console.log(data3);
-
-//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-//         console.log('Your file has been written 😁');
-//       })
-//     });
-//   });
-// });
-// console.log('Will read file!');
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
@@ -38,6 +12,8 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 // Server
 
 const server = http.createServer((req, res) => {
